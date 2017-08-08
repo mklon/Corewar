c846c1344bb400db4938c77c33331926a650a623
Alexey <mKlon9@gmail.com>
2017-08-08 15:59:31 +0300

    ext

--- new.c
+++ new.c
@@ -10,112 +10,100 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <GLUT/GLUT.h>
- 
 #include <stdlib.h>
- 
-// инициализация переменных цвета в 1.0
-// треугольник - белый
-float red = 1.0f, blue = 1.0f, green = 1.0f;
- 
-// угол поворота
-float angle = 0.0f;
- 
-void changeSize(int w, int h)
+#include <unistd.h>
+#include <GLUT/GLUT.h>
+
+void *font = GLUT_BITMAP_HELVETICA_18;
+
+char	*str;
+
+void	draw_string_bitmap(void *font, const char* str)
 {
-	// предотвращение деления на ноль
-	if (h == 0)
-		h = 1;
-	float ratio =  w * 1.0 / h;
-	// используем матрицу проекции
-	glMatrixMode(GL_PROJECTION);
-	// обнуляем матрицу
-	glLoadIdentity();
-	// установить параметры вьюпорта
-	glViewport(0, 0, w, h);
-	// установить корректную перспективу
-	gluPerspective(45.0f, ratio, 0.1f, 100.0f);
-	// вернуться к матрице проекции
-	glMatrixMode(GL_MODELVIEW);
+	int		i;
+	float	x;
+	float	y;
+
+	i = 1;
+	x = -0.98;
+	y = 0.93;
+	while (*str)
+	{
+		glRasterPos2f(x, y);
+		glutBitmapCharacter(font, *str++);
+		glutBitmapCharacter(font, *str++);
+		if (i % 32 == 0)
+		{
+			y -= 0.03;
+			x = -1.02;
+		}
+		x += 0.04;
+		i++;
+	}
 }
- 
-void renderScene(void) {
+
+void	renderScene(void) {
  
 	// очистить буфер цвета и глубины.
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 	// обнулить трансформацию
 	glLoadIdentity();
 	// установить камеру
-	gluLookAt( 0.0f, 0.0f, 10.0f,
-		   0.0f, 0.0f,  0.0f,
-		   0.0f, 1.0f,  0.0f);
-	//поворот на заданную величину
-	//glRotatef(angle, 0.0f, 1.0f, 0.0f);
-	// установить цвет модели
-	glColor3f(red,green,blue);
+	glColor3f(0.4, 0.4, 0.4);
 	glLineWidth(10);
-	glPointSize(9);
+	glPointSize(8);
+	glEnable(GL_LINE_SMOOTH);
 	glBegin(GL_LINE_LOOP);
-		glVertex2d(-7.5, 4);
-		glVertex2d(4.5, 4);
-		glVertex2d(4.5, -4);
-		glVertex2d(-7.5, -4);
+		glVertex2d(-0.99, 0.98);
+		glVertex2d(0.60, 0.98);
+		glVertex2d(0.60, -0.98);
+		glVertex2d(-0.99, -0.98);
 	glEnd();
 	glEnable(GL_POINT_SMOOTH);
 	glBegin(GL_POINTS);
-		glVertex2d(-7.5, 4);
-		glVertex2d(4.5, 4);
-		glVertex2d(4.5, -4);
-		glVertex2d(-7.5, -4);
+		glVertex2d(-0.99, 0.98);
+		glVertex2d(0.60, 0.98);
+		glVertex2d(0.60, -0.98);
+		glVertex2d(-0.99, -0.98);
 	glEnd();
- 
-	angle+=0.1f;
- 
+	glColor3f(0, 1, 1);
+	glLineWidth(17);
+	glBegin(GL_LINES);
+		glColor3f(0, 0, 1);
+		glVertex2d(-0.98, 0.94);
+		glColor3f(0, 1, 0);
+		glVertex2d(-0.92, 0.94);
+		glVertex2d(-0.98, 0.91);
+		glColor3f(1, 0, 0);
+		glVertex2d(-0.92, 0.91);	
+	glEnd();
+	glColor3f(0.7, 0.7, 0.7);
+	draw_string_bitmap(font, str);
 	glutSwapBuffers();
 }
  
-void processNormalKeys(unsigned char key, int x, int y) {
+void	processNormalKeys(unsigned char key, int x, int y) {
  
 	if (key == 27)
 		exit(0);
 }
- 
-void processSpecialKeys(int key, int x, int y) {
- 
-	switch(key)
-	{
-		case GLUT_KEY_LEFT :
-				red = 1.0;
-				green = 0.0;
-				blue = 0.0; break;
-		case GLUT_KEY_RIGHT :
-				red = 0.0;
-				green = 1.0;
-				blue = 0.0; break;
-		case GLUT_KEY_UP :
-				red = 0.0;
-				green = 0.0;
-				blue = 1.0; break;
-	}
-}
 
-int main(int argc, char **argv)
+int		main(int argc, char **argv)
 {
 	// инициализация
 	glutInit(&argc, argv);
 	glutInitDisplayMode(GLUT_DEPTH | GLUT_DOUBLE | GLUT_RGBA);
-	glutInitWindowPosition(0,0);
 	glutInitWindowSize(5120,2880);
-	glutCreateWindow("Урок 4");
-
+	glutCreateWindow("COREWAR");
+	
+	str = (char*)malloc(sizeof(char) * 4096 + 1);
+	for (int i = 0; i < 4096; i++)
+		str[i] = '0';
 	// регистрация
 	glutDisplayFunc(renderScene);
-	glutReshapeFunc(changeSize);
-	glutIdleFunc(renderScene);
 
 	// наши новые функции
 	glutKeyboardFunc(processNormalKeys);
-	glutSpecialFunc(processSpecialKeys);
 
 	// основной цикл
 	glutMainLoop();
