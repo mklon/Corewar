83562edc0c28f53ab88f784a5ba39d87e377b1f4
NataliYatsulka <yatsulka@gmail.com>
2017-10-12 14:12:30 +0300

    usage

--- vm/ft_usage.c
+++ vm/ft_usage.c
@@ -15,11 +15,14 @@
 void	ft_usage(void)
 {
 	ft_printf("\n");
-	ft_printf("Usage: ./corewar [-d N | -v] [-a] [[-n N] <champion1.cor>] <champion1.cor> <...>\n");
-	ft_printf("    -a        : Prints output from \"aff\" (Default is to hide it)\n");
+	ft_printf("Usage: ./corewar [-d N | -v] [-a] [[-n N] ");
+	ft_printf("<champion1.cor>] <champion1.cor> <...>\n");
+	ft_printf("    -a        : Prints output from \"aff\" ");
+	ft_printf("(Default is to hide it)\n");
 	ft_printf("    -d N      : Dumps memory after N cycles then exits\n");
 	ft_printf("    -v        : Ncurses output mode\n");
 	ft_printf("    -n N      : sets the number N of the next player\n");
+	ft_printf("    -g        : Show operations (DEBUG)\n");
 	ft_printf("\n");
 	exit(0);
 }
